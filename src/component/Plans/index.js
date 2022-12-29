4f84305da4d8f0743b64cbfb425b303981b61de4
= <luis@up4tech.com.br>
2022-12-28 21:11:14 -0300

    layout home

---
+++ src/component/Plans/index.js
@@ -0,0 +1,14 @@
+function Plans(){
+    return (
+        <section className='container'>
+            <div className='container-items'>
+                <div className='container-describle-new'>
+                    <h4>Novo!</h4>
+                </div>
+                <p>Planos a partir de <b> R$ 18,90</b>.</p>
+                <h3>Saiba mais</h3>
+            </div>
+        </section>
+    )
+}
+export default Plans;
