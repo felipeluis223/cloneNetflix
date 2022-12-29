dc854fe1b3276d8455d93e662b317f9ca80d2f59
= <luis@up4tech.com.br>
2022-12-29 11:08:38 -0300

    componente resposável por criar cada item de novidade

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import './App.css';
 import Content from './component/Content';
+import ItemContent from './component/ItemContent';
 import Plans from './component/Plans';
 
 function App() {
+
+  const allData = [
+    {
+      title: "Aproveite na TV.",
+      description: "Assista em Smart TVs, PlayStation, Xbox, Chromecast, Apple TV, aparelhos de Blu-ray e outros dispositivos.",
+      side: "left",
+      img: "tv",
+    },
+
+    {
+      title: "Assista quando quiser.",
+      description: "Assista no celular, tablet, Smart TV ou notebook sem pagar a mais por isso.",
+      side: "right",
+      img: "devicePile",
+    },
+
+    {
+      title: "Crie perfis para crianças.",
+      description: "Deixe as crianças se aventurarem com seus personagens favoritos em um espaço feito só para elas, sem pagar a mais por isso.",
+      side: "left",
+      img: "kids",
+    },
+
+    {
+      title: "Baixe séries para assistir offline.",
+      description: "Disponível em todos os planos, exceto no Básico com anúncios.",
+      side: "right",
+      img: "mobile",
+    },
+  ]
+
   return (
     <div className="App">
       <Plans />
       <Content />
-      <section style={{width: "100%", height: 40, backgroundColor: "red"}}>segunda sessão</section>
+      {
+        allData.map((item, index)=>(
+          <ItemContent 
+            title={item.title}
+            description={item.description}
+            side={item.side}
+            img={item.img}
+          />
+        ))
+      }
     </div>
   );
 }
---
+++ src/component/ItemContent/index.js
@@ -0,0 +1,48 @@
+import imgTv from './img/img-tv.png';
+import imgDevicePile from './img/img-device-pile.png';
+import imgKids from './img/img-kids.png';
+import imgMobile from './img/img-mobile.jpg';
+
+function ItemContent({ title, description, side, img }){
+    let chooseImage;
+    switch(img){
+        case "tv": 
+            chooseImage = imgTv;
+            break
+        case "devicePile": 
+            chooseImage = imgDevicePile;
+            break
+
+        case "kids": 
+            chooseImage = imgKids;
+            break
+        case "mobile": 
+            chooseImage = imgMobile;
+            break
+    }
+
+    return (
+        <section style={{
+            marginBottom: 10,
+            width: "100%",
+            height: 425,
+            backgroundColor: "#000000",
+            display: "flex",
+            justifyContent: "space-evenly",
+            flexDirection: (side === "left" ? "row" : "row-reverse")
+        }}>
+            <div className='container-tv-description'>
+                <h2>{title}</h2>
+                <span>{description}</span>
+            </div>
+            <div className='container-tv-description'>
+                <img 
+                    src={chooseImage}
+                    className='device'
+                />
+            </div>
+        </section>
+    );
+}
+
+export default ItemContent;
